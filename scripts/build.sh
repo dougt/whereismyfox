3867f64fdbab23ca1927191ee831d2d50ef6dd43
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2013-06-05 14:09:25 -0700

    Stop generating package.zip

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,2 @@
-
 rm -f whereismyfox
 GOPATH=`pwd` go build whereismyfox.com/whereismyfox
-
-pushd app
-zip -u package.zip \
-    index.html \
-    manifest.webapp \
-    logos/64.png \
-    logos/128.png
-popd
