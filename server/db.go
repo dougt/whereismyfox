3b37f030236d23a990943052a5b9d88bb4424b57
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2013-07-31 16:33:28 -0300

    Server-side changes for commands.
    The server stores the recognized commands in a 'commands' table,
    and keeps track of which devices implement which commands in a
    'device_has_command' table.
    
    The 'commands' table is initialized when the server is started
    by reading the commands.json file.
    
    The server issues a push notification for the device to trigger
    a command and saves the context of the command (that is, command and
    arguments) in memory waiting for the device to fetch it.
    
    The will device the command ids it supports to the server every time
    the app starts up.

--- server/db.go
+++ server/db.go
@@ -5,6 +5,12 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type Command struct {
+	Id          int64  `json: "id"`
+	Name        string `json: "name"`
+	Description string `json: "description"`
+}
+
 type Device struct {
 	User      string
 	Id        int64   `json: "id"`
@@ -36,6 +42,36 @@ func OpenDB(dbpath string) (*DB, error) {
 		return nil, err
 	}
 
+	_, err = conn.Exec(
+		`drop table if exists commands`)
+
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = conn.Exec(
+		`create table commands
+		(id integer primary key, name text, description text,
+		unique (id, name, description))`)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// FIXME sqlite3 seems to need a special pragma to enforce
+	// foreign keys, so the go-sql abstraction will have to leak
+	// here somehow.
+	// https://www.sqlite.org/foreignkeys.html#fk_enable
+	_, err = conn.Exec(
+		`create table if not exists commands_for_device
+		(device_id integer references devices(id),
+		command_id integer references commands(id),
+		primary key (device_id, command_id))`)
+
+	if err != nil {
+		return nil, err
+	}
+
 	return &DB{conn}, nil
 }
 
@@ -54,7 +90,45 @@ func (self DB) AddDevice(user, name, endpoint string) (*Device, error) {
 	}
 
 	id, _ := res.LastInsertId()
-	return &Device{Id: id, User: user, Name: name, Endpoint: endpoint}, nil
+	return &Device{Id: id, Name: name, User: user, Endpoint: endpoint}, nil
+}
+
+func (self DB) AddCommand(id int64, name, description string) (*Command, error) {
+	_, err := self.connection.Exec(
+		`insert into commands(id, name, description) values(?, ?, ?)`,
+		id, name, description)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Command{Id: id, Name: name, Description: description}, nil
+}
+
+func (self DB) AddCommandForDevice(device, command int64) error {
+	_, err := self.connection.Exec(
+		`insert into commands_for_device(device_id, command_id)
+		values(?, ?)`, device, command)
+
+	return err
+}
+
+func (self DB) UpdateCommandsForDevice(device int64, commands []int64) error {
+	_, err := self.connection.Exec(
+		`delete from commands_for_device where device_id=?`, device)
+
+	if err != nil {
+		return err
+	}
+
+	// FIXME should be a transaction?
+	for _, cmdid := range commands {
+		if err = self.AddCommandForDevice(device, cmdid); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (self DB) GetDeviceById(id int64) (*Device, error) {
@@ -73,6 +147,32 @@ func (self DB) GetDeviceById(id int64) (*Device, error) {
 	}
 
 	return &d, nil
+}
+
+func (self DB) ListCommandsForDevice(d *Device) ([]*Command, error) {
+	res, err := self.connection.Query(
+		`select id, name, description
+		from (commands, commands_for_device)
+		where commands.id = commands_for_device.command_id
+		and commands_for_device.device_id=?`, d.Id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	commands := []*Command{}
+	for res.Next() {
+		c := Command{}
+		err = res.Scan(&c.Id, &c.Name, &c.Description)
+
+		if err != nil {
+			return nil, err
+		}
+
+		commands = append(commands, &c)
+	}
+
+	return commands, nil
 }
 
 func (self DB) UpdateDeviceLocation(device *Device, latitude, longitude float64) error {
